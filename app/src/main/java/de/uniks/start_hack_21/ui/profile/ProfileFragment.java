fcefb2eb1bb2c383ca8bd025a3c812477101614f
Schoene94 <nils.schoenewolf@student.uni-kassel.de>
2021-03-20 22:48:38 +0100

    feat: working friendslist

--- app/src/main/java/de/uniks/start_hack_21/ui/profile/ProfileFragment.java
+++ app/src/main/java/de/uniks/start_hack_21/ui/profile/ProfileFragment.java
@@ -2,21 +2,25 @@ package de.uniks.start_hack_21.ui.profile;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,8 +35,6 @@ public class ProfileFragment extends Fragment {
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        LinearLayout container_overview = root.findViewById(R.id.container_overview);
-        LinearLayout container_history = root.findViewById(R.id.container_history);
         LinearLayout container_friends = root.findViewById(R.id.container_friends);
 
         PieChart pieChartFirst = root.findViewById(R.id.pieChart_view_first);
@@ -43,6 +45,8 @@ public class ProfileFragment extends Fragment {
         fillPieChart(pieChartFirst, 64, "#ffa500");
         fillPieChart(pieChartSecond, 76, "#3ca567");
         fillPieChart(pieChartThird, 81, "#3ca567");
+
+        fillFriends(container_friends, inflater);
 
         return root;
     }
@@ -69,5 +73,24 @@ public class ProfileFragment extends Fragment {
 
         pieChart.setData(pieData);
         pieChart.invalidate();
+    }
+
+    private void fillFriends(LinearLayout friendsContainer, LayoutInflater inflater) {
+        HashMap<String, String> friendsMap = new HashMap<>();
+        friendsMap.put("Florian Microsoft", "42");
+        friendsMap.put("Steffen Meister", "-1");
+        friendsMap.put("Torbinho", "9000");
+
+        for (String friend : friendsMap.keySet()) {
+            View view = inflater.inflate(R.layout.component_friends_card, null);
+            TextView nameView = view.findViewById(R.id.text_friend_name);
+            TextView levelView = view.findViewById(R.id.text_friend_level);
+
+            nameView.setText(friend);
+            levelView.setText(friendsMap.get(friend));
+
+            friendsContainer.addView(view);
+        }
+
     }
 }
