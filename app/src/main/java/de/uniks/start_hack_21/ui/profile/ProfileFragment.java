0a7a8830530603bd452d1ef468ee9c55c6e567a6
Julian Holfeld <julianholfeld@student.uni-kassel.de>
2021-03-20 21:07:06 +0100

    feat: Added PieCharts to the profile-fragment

--- app/src/main/java/de/uniks/start_hack_21/ui/profile/ProfileFragment.java
+++ app/src/main/java/de/uniks/start_hack_21/ui/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package de.uniks.start_hack_21.ui.profile;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,15 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.uniks.start_hack_21.R;
 
@@ -25,9 +35,39 @@ public class ProfileFragment extends Fragment {
         LinearLayout container_history = root.findViewById(R.id.container_history);
         LinearLayout container_friends = root.findViewById(R.id.container_friends);
 
+        PieChart pieChartFirst = root.findViewById(R.id.pieChart_view_first);
+        PieChart pieChartSecond = root.findViewById(R.id.pieChart_view_second);
+        PieChart pieChartThird = root.findViewById(R.id.pieChart_view_third);
 
+        // never hardcode colors like that children
+        fillPieChart(pieChartFirst, 64, "#ffa500");
+        fillPieChart(pieChartSecond, 76, "#3ca567");
+        fillPieChart(pieChartThird, 81, "#3ca567");
 
         return root;
     }
 
+    private void fillPieChart(PieChart pieChart, int percentage, String color) {
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+        String label = "type";
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.parseColor(color));
+        colors.add(Color.parseColor("#808080"));
+
+        String first = percentage + "%";
+        pieEntries.add(new PieEntry(percentage, first));
+        pieEntries.add(new PieEntry(100 -percentage));
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, label);
+        pieDataSet.setValueTextSize(12f);
+        pieDataSet.setColors(colors);
+        PieData pieData = new PieData(pieDataSet);
+        pieData.setDrawValues(false);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.getDescription().setEnabled(false);
+
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+    }
 }
