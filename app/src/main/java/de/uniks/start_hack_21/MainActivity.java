4a853defd61c868062c400cefbac6bf551c30b7f
Steffen K <steffenkrapf@gmail.com>
2021-03-20 17:49:11 +0100

    added test data as json and deserialization

--- app/src/main/java/de/uniks/start_hack_21/MainActivity.java
+++ app/src/main/java/de/uniks/start_hack_21/MainActivity.java
@@ -1,11 +1,8 @@
 package de.uniks.start_hack_21;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -15,6 +12,9 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import de.uniks.start_hack_21.data.User;
+import de.uniks.start_hack_21.util.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        User user = Utils.loadData(getApplicationContext());
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
---
+++ app/src/main/java/de/uniks/start_hack_21/data/PulseData.java
@@ -0,0 +1,39 @@
+package de.uniks.start_hack_21.data;
+
+public class PulseData {
+    private String Date;
+    private int pulseAverage;
+    private int percentLow;
+    private int percentMid;
+    private int percentHigh;
+    private int max;
+    private int min;
+
+    public String getDate() {
+        return Date;
+    }
+
+    public int getPulseAverage() {
+        return pulseAverage;
+    }
+
+    public int getPercentLow() {
+        return percentLow;
+    }
+
+    public int getPercentMid() {
+        return percentMid;
+    }
+
+    public int getPercentHigh() {
+        return percentHigh;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
